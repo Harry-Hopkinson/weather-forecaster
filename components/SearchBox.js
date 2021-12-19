a2009eba861c38d72960cd59b9a04f1c63b6823b
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-19 21:08:53 +0000

    Search bar now works

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -3,6 +3,7 @@ import cities from "../lib/city.list.json";
 
 export default function SearchBox() {
     const [query, setQuery] = React.useState("");
+    const [results, setResults] = React.useState([]);
 
     const onChange = (e) => {
         const { value } = e.target;
@@ -10,22 +11,37 @@ export default function SearchBox() {
         let matchingCities = [];
 
         // Loops through the cities array and checks if the query, if longer than 3, is in the array.
-        if (value.length > 3) {
+        if (value.length > 0) {
             for (let city of cities) {
                 if (matchingCities.length >= 5) {
                     break;
                 }
+
                 const match = city.name.toLowerCase().startsWith(value.toLowerCase());
                 if (match) {
                     matchingCities.push(city);
                 }
             }
         }
+
+        return setResults(matchingCities);
     };
 
     return (
         <div className="search">
             <input type="text" value={query} onChange={onChange}/>
+
+            {query.length > 0 && results.length > 0 && (
+                <ul>
+                    {results.map((result) => (
+                        <li key={result.id}>
+                            <a href={`/city/${result.id}`}>
+                                {result.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }
