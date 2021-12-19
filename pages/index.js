385bb9560c5ffcc9dfe2cf6c3da53f8ca88fbff6
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-19 18:55:17 +0000

    Added a styled input box

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
+import SearchBox from '../components/SearchBox'
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Head>
+        <title>Weather Forecaster</title>
+      </Head>
+
+      <div className='home'>
+        <div className='container'>
+          <SearchBox />
+          {/* */}
+        </div>
+      </div>
     </div>
   )
 }
