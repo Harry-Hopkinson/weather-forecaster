d0a8d9045ce1d2f0e7dc8e02f9e352bab4c13161
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-19 19:00:02 +0000

    Basic Input Working

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import city from "../lib/city.list.json";
 
 export default function SearchBox() {
+    const [query, setQuery] = React.useState("");
+
+    const onChange = (e) => {
+        const { value } = e.target;
+        setQuery(value);
+    };
+
     return (
         <div className="search">
-            <input type="text" />
+            <input type="text" value={query} onChange={onChange}/>
         </div>
     )
 }
