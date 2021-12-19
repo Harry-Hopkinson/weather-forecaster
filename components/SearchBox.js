a0e572e470fbe3eea777b187b5bf6e0f5e00960b
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-19 19:04:30 +0000

    Started a loop through the cities

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import city from "../lib/city.list.json";
+import cities from "../lib/city.list.json";
 
 export default function SearchBox() {
     const [query, setQuery] = React.useState("");
@@ -7,6 +7,20 @@ export default function SearchBox() {
     const onChange = (e) => {
         const { value } = e.target;
         setQuery(value);
+        let matchingCities = [];
+
+        // Loops through the cities array and checks if the query, if longer than 3, is in the array.
+        if (value.length > 3) {
+            for (let city of cities) {
+                if (matchingCities.length >= 5) {
+                    break;
+                }
+                const match = city.name.toLowerCase().startsWith(value.toLowerCase());
+                if (match) {
+                    matchingCities.push(city);
+                }
+            }
+        }
     };
 
     return (
