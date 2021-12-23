3ab291d44533e5701767a4b15b4cc2ff9a97e1cc
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-23 09:06:53 +0000

    The city's API is now printed.

--- pages/location/[city].js
+++ pages/location/[city].js
@@ -1,7 +1,9 @@
 import React from 'react';
+import cities from "../../lib/city.list.json";
 
 export async function getServerSideProps(context) {
-    const cityID = getCity(context.params.city);
+    const city = getCity(context.params.city);
+    console.log(city);
     const slug = context.params.city;
 
     return {
@@ -16,6 +18,20 @@ const getCity = param => {
     // get the id of the city by splitting the API
     const splitCity = cityParam.split('-');
     const id = splitCity[splitCity.length - 1];
+    console.log(id);
+
+    if (!id) {
+        return null;
+    }
+
+    const city = cities.find(city => city.id.toString() == id);
+
+    if (city) {
+        return city;
+    }
+    else {
+        return null;
+    }
 }
 
 export default function City({slug}) {
